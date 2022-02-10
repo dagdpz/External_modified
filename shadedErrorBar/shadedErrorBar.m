346161b5b635e66666247f2114de7be417a500a0
luschn <lukas.schneider.m@gmail.com>
2022-02-10 13:35:09 +0100

    Update shadedErrorBar.m

--- shadedErrorBar/shadedErrorBar.m
+++ shadedErrorBar/shadedErrorBar.m
@@ -1,5 +1,8 @@
 function varargout=shadedErrorBar(x,y,errBar,lineProps,transparent)
 matlabversion=datevec(version('-date'));
+if nargin < 5
+   transparent=0; 
+end
 if matlabversion(1)>=2014
     out=shadedErrorBar_2014(x,y,errBar,'lineprops',lineProps,'transparent',transparent);
 else
