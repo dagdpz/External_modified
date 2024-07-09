175e268e9e2d483c6507aaa4fc7f28c84392d729
lubavas <19555010+lubavas@users.noreply.github.com>
2024-07-09 14:22:01 +0200

    export fig in vector

--- export_fig.m
+++ export_fig.m
@@ -1,7 +1,7 @@
 function export_fig(varargin)
 matlabversion=datevec(version('-date'));
 if matlabversion(1)>=2020
-    exportgraphics(varargin{1}, [varargin{2} '.pdf']) % with all respect to export_fig and previous efforts of people to make it work, let's use the existing funtionality of Matlab for saving pdfs
+    exportgraphics(varargin{1}, [varargin{2} '.pdf'],'ContentType','vector') % with all respect to export_fig and previous efforts of people to make it work, let's use the existing funtionality of Matlab for saving pdfs
 elseif matlabversion(1)>=2014
     export_fig_2014(varargin{:});    
 else
